ae85ef520e2b011ceefe21b3789d31266de3061d
maitt <ngaychotnang@gmail.com>
2017-05-27 09:55:43 +0700

    CI-02

--- CI/js/main.js
+++ CI/js/main.js
@@ -1,59 +1,92 @@
 var Nakama = {};
 Nakama.configs = {
+    bulletSpeed: 700,
+    shipSpeed: 500,
     mapSpeed: 5
 };
 
-window.onload = function(){
-  Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
-    {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    }, false, false
-  );
+
+window.onload = function() {
+    Nakama.game = new Phaser.Game(640, 960, Phaser.AUTO, '', {
+        preload: preload,
+        create: create,
+        update: update,
+        render: render
+    }, false, false);
 }
 
 // preparations before game starts
-var preload = function(){
-  Nakama.game.scale.minWidth = 320;
-  Nakama.game.scale.minHeight = 480;
-  Nakama.game.scale.maxWidth = 640;
-  Nakama.game.scale.maxHeight = 960;
-  Nakama.game.scale.pageAlignHorizontally = true;
-  Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+var preload = function() {
+    Nakama.game.scale.minWidth = 320;
+    Nakama.game.scale.minHeight = 480;
+    Nakama.game.scale.maxWidth = 640;
+    Nakama.game.scale.maxHeight = 960;
+    Nakama.game.scale.pageAlignHorizontally = true;
+    Nakama.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 
-  Nakama.game.time.advancedTiming = true;
+    Nakama.game.time.advancedTiming = true;
 
-  Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
-  Nakama.game.load.image('background', 'Assets/Map1.png');
+    Nakama.game.load.atlasJSONHash('assets', 'Assets/assets.png', 'Assets/assets.json');
+    Nakama.game.load.image('background', 'Assets/Map1.png');
 }
 
 // initialize the game
-var create = function(){
-  Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
-  Nakama.keyboard = Nakama.game.input.keyboard;
-  Nakama.map = Nakama.game.add.tileSprite(0, 0, 640, 960, 'background');
-  //var background = Nakama.game.add.sprite(0, 0, 'background');
+var create = function() {
 
-  Nakama.player = Nakama.game.add.sprite(300, 400, 'assets',"Spaceship1-Player.png");
+    Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
+    Nakama.keyboard = Nakama.game.input.keyboard;
+
+    Nakama.map = Nakama.game.add.tileSprite(0, 0, 640, 960, 'background');
+    Nakama.shipBulletGroup = Nakama.game.add.physicsGroup();
+    Nakama.playerGroup = Nakama.game.add.physicsGroup();
+
+    // players
+    Nakama.players = [];
+    Nakama.players.push(
+        new ShipController(
+            Nakama.game.world.centerX - 36 + 50,
+            Nakama.game.world.centerY + 200,
+            "Spaceship1-Player.png", {
+                up: Phaser.Keyboard.UP,
+                down: Phaser.Keyboard.DOWN,
+                left: Phaser.Keyboard.LEFT,
+                right: Phaser.Keyboard.RIGHT,
+                fire: Phaser.Keyboard.CONTROL,
+                cooldown: 0.15
+            }
+        )
+    );
+    Nakama.players.push(
+        new ShipController(
+            Nakama.game.world.centerX - 36 - 50,
+            Nakama.game.world.centerY + 200,
+            "Spaceship1-Partner.png", {
+                up: Phaser.Keyboard.W,
+                down: Phaser.Keyboard.S,
+                left: Phaser.Keyboard.A,
+                right: Phaser.Keyboard.D,
+                fire: Phaser.Keyboard.SPACEBAR,
+                cooldown: 0.15
+            }
+        )
+    );
+
 }
 
 // update game state each frame
-var update = function(){
-  //scrolling map
- Nakama.map.tilePosition.y += Nakama.configs.mapSpeed;
- if(Nakama.keyboard.isDown(Phaser.Keyboard.UP)){
-   Nakama.player.position.y = Math.max(Nakama.player.position.y - 10, 0);
- } else if(Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)){
-   Nakama.player.position.y = Math.min(Nakama.player.position.y + 10, Nakama.game.height - 78);
- }
- if(Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)){
-   Nakama.player.position.x = Math.max(Nakama.player.position.x - 10, 0);
- }else if(Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-   Nakama.player.position.x = Math.min(Nakama.player.position.x + 10, Nakama.game.width - 78);
- }
+var update = function() {
+    //scrolling map
+    Nakama.map.tilePosition.y += Nakama.configs.mapSpeed;
+
+    for (var i = 0; i < Nakama.players.length; i++) {
+        Nakama.players[i].update();
+    }
+}
+
+var bulletPlayerCollider = function(bullet, player) {
+    bullet.kill();
+    player.kill();
 }
 
 // before camera render (mostly for debug)
-var render = function(){}
+var render = function() {}
